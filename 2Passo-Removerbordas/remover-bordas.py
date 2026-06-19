cd68add952d07de26612e338ac9b24740fa9fdce
C4ua09 <caua.costa09@escola.pr.gov.br>
2026-06-19 11:13:47 -0300

    removi as bordas

--- 2Passo-Removerbordas/remover-bordas.py
+++ 2Passo-Removerbordas/remover-bordas.py
@@ -25,7 +25,7 @@ for nome_arquivo in os.listdir(pasta_imagens):
 
         largura, altura = imagem.size
 
-        caixa_corte = (566, 122, largura - 2389, altura - 2160) # ATUALIZE AQUI OS VALORES DE CORTE (esquerda, superior, direita, inferior)
+        caixa_corte = (565, 141, largura - 564, altura - 0) # ATUALIZE AQUI OS VALORES DE CORTE (esquerda, superior, direita, inferior)
         imagem_cortada = imagem.crop(caixa_corte)
 
         caminho_saida = os.path.join(pasta_saida, nome_arquivo)
